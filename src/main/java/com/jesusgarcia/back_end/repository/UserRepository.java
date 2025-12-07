932da8124b1a1d49df55a3f31e4bd0e302b9c049
Jesus Garcia <jesgarjimher@gmail.com>
2025-12-07 12:27:43 +0100

    Cleaning duplicated files from old root

--- src/main/java/com/jesusgarcia/back_end/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jesusgarcia.back_end.repository;
-
-import com.jesusgarcia.back_end.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-
-}
