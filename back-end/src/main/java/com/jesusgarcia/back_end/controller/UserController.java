c70e4391930569373b9a02eeb58824f40f70dba0
Jesus Garcia <jesgarjimher@gmail.com>
2025-12-08 11:44:47 +0100

    CORS policy changed, mapping dynamic data

--- back-end/src/main/java/com/jesusgarcia/back_end/controller/UserController.java
+++ back-end/src/main/java/com/jesusgarcia/back_end/controller/UserController.java
@@ -3,11 +3,17 @@ package com.jesusgarcia.back_end.controller;
 import com.jesusgarcia.back_end.model.User;
 import com.jesusgarcia.back_end.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -16,5 +22,10 @@ public class UserController {
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
