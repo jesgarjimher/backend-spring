7d32ffcd51307ce9682b2eab7b54576bed426cd1
Jesus Garcia <jesgarjimher@gmail.com>
2025-12-08 14:52:54 +0100

    added get/user/id, added exception

--- back-end/src/main/java/com/jesusgarcia/back_end/controller/UserController.java
+++ back-end/src/main/java/com/jesusgarcia/back_end/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.jesusgarcia.back_end.controller;
 
+import com.jesusgarcia.back_end.exception.UserNotFoundException;
 import com.jesusgarcia.back_end.model.User;
 import com.jesusgarcia.back_end.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -27,5 +29,11 @@ public class UserController {
     @GetMapping("/users")
     List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() ->new UserNotFoundException(id));
     }
 }
