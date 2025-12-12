d1a06d66a85dfba97e9f9389554474a68460b081
Jesus Garcia <jesgarjimher@gmail.com>
2025-12-12 11:57:15 +0100

    delete functionality added to the front

--- front-end/src/layout/pages/Home.js
+++ front-end/src/layout/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export default function Home() {
 
@@ -10,9 +10,16 @@ export default function Home() {
         loadUsers();
     },[]); //solo se ejecuta una vez
 
+    const {id} = useParams();
+
     const loadUsers = async () => {
         const result = await axios.get("http://localhost:8080/users")
         setUsers(result.data);
+    }
+
+    const deleteUser = async (id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`)
+        loadUsers();
     }
 
     return(
@@ -41,7 +48,7 @@ export default function Home() {
             <td><Link className="btn btn-outline-primary mx-2"
             to={`/edituser/${user.id}`}
             >Edit</Link></td>
-            <td><button className="btn btn-danger mx-2">Delete</button></td>
+            <td><button className="btn btn-danger mx-2" onClick={()=> deleteUser(user.id)}>Delete</button></td>
         </tr>
         
     })
